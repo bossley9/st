df96997aa829a491a0a5028893f42b2d7a98b65f
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-08-31 11:19:04 -0400

    Update font to system variable

--- template.config.h
+++ template.config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro Medium:pixelsize=15:antialias=true:autohint=true";
+static char *font = "$FONT";
 static int borderpx = $WM_GAPS;
 
 /*

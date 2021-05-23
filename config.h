dc0eb45b245239a9ae1f6abefd05c8500fec4a41
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-05-23 11:45:21 -0700

    Remove /bin/sh comment

--- config.h
+++ config.h
@@ -21,7 +21,6 @@ static int borderpx = 30;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-/* static char *shell = "/bin/sh"; */
 static char *shell = "usr/bin/env sh";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
